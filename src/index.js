2eee85aac1cba9f133c2dbd92eb12cabfa225bd8
Tminus0 <Nikhil876706@gmail.com>
2024-07-29 09:55:50 +0530

    server change

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const logger = require('./config/logger');
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
+  server = app.listen(config.port,'0.0.0.0', () => {
+    console.log(`Server is listening on http://0.0.0.0:${config.port}`);
   });
 });
 
